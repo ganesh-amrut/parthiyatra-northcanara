5a3e488635ab18136fc8e35fe5a8df1634be6248
ganesh-amrut <161237488+ganesh-amrut@users.noreply.github.com>
2024-02-27 21:28:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const homeLink = document.getElementById('home-link');
+    const routesLink = document.getElementById('routes-link');
+    const bookingsLink = document.getElementById('bookings-link');
+    const devoteesLink = document.getElementById('devotees-link');
+    const contentContainer = document.getElementById('content');
+
+    homeLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        loadContent('home.html');
+    });
+
+    routesLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        loadContent('routes.html');
+    });
+
+    bookingsLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        loadContent('bookings.html');
+    });
+
+    devoteesLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        loadContent('devotees.html');
+    });
+
+    function loadContent(page) {
+        fetch(page)
+            .then(response => response.text())
+            .then(html => {
+                contentContainer.innerHTML = html;
+            })
+            .catch(error => console.error('Error loading content:', error));
+    }
+});
